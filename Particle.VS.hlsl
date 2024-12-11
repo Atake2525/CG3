c8dbeafc682de0e162b72dee84ace884da0097b8
Atake <146906117+Atake2525@users.noreply.github.com>
2024-12-11 17:11:39 +0900

    pipelineStateができない

--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -3,11 +3,12 @@
 
 
 
-struct TransformationMatrix{
+struct ParticleForGPU{
     float32_t4x4 WVP;
     float32_t4x4 World;
+    float32_t4 color;
 };
-StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
+StructuredBuffer<ParticleForGPU> gParticle : register(t0);
 
 //struct VertexShaderOutput{
 //    float32_t4 position : SV_POSITION;
@@ -15,16 +16,15 @@ StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
 
 struct VertexShaderInput{
    // float32_t4 position : POSITION0;
-    float32_t4 position : POSITION0;
+    float32_t4 position : SV_POSITION;
     float32_t2 texcoord : TEXCOORD0;
-    float32_t3 normal : NORMAL0;
 };
 
 VertexShaderOutput main(VertexShaderInput input, uint32_t instanceId : SV_InstanceID){
     VertexShaderOutput output;
-    output.position = mul(input.position, gTransformationMatrices[instanceId].WVP);
+    output.position = mul(input.position, gParticle[instanceId].WVP);
     output.texcoord = input.texcoord;
-    output.normal = normalize(mul(input.normal, (float3x3) gTransformationMatrices[instanceId].World));
+    output.color = gParticle[instanceId].color;
     return output;
 }
 
