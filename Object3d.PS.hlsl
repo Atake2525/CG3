93a2dc124d5018721429c5817d9d32276db1c9f9
Atake <146906117+Atake2525@users.noreply.github.com>
2025-01-24 12:07:27 +0900

    文字化けの修正

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -7,69 +7,68 @@ SamplerState gSampler : register(s0);
     float32_t4 color : SV_TARGET0;
 };*/
 
-struct Material {
+struct Material
+{
     float32_t4 color;
     int32_t enableLighting;
     float32_t4x4 uvTransform;
     float32_t shininess;
 };
 ConstantBuffer<Material> gMaterial : register(b0);
-struct PixelShaderOutput {
+struct PixelShaderOutput
+{
     float32_t4 color : SV_TARGET0;
 };
 
-struct DirectionalLight {
-    float32_t4 color; //!< ���C�g�̐F
-    float32_t3 direction; //!< ���C�g�̌���
-    float intensity; //!< �P�x
+struct DirectionalLight
+{
+    float32_t4 color; //!< ライトの色
+    float32_t3 direction; //!< ライトの向き
+    float intensity; //!< 輝度
 };
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 
-struct Camera {
+struct Camera
+{
     float32_t3 worldPosition;
 };
 ConstantBuffer<Camera> gCamera : register(b2);
 
-PixelShaderOutput main(VertexShaderOutput input){
+PixelShaderOutput main(VertexShaderOutput input)
+{
     PixelShaderOutput output;
-    float32_t4 transformedUV = mul(float3x2_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
+    output.color = gMaterial.color;
+    float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     
-    if (gMaterial.enableLighting != 0){ // Lighting����ꍇ
+    if (gMaterial.enableLighting != 0)
+    { // Lightingする場合
         float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
         
         // Phong Reflection Model
-        // �v�Z�� R = reflect(L,N) specular = (V.R)n
+        // 計算式 R = reflect(L,N) specular = (V.R)n
         float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
         float32_t3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
     
         float RdotE = dot(reflectLight, toEye);
-        float specularPow = pow(saturate(RdotE), gMaterial.shininess); // ���ˋ��x
+        float specularPow = pow(saturate(RdotE), gMaterial.shininess); // 反射強度
         
-        // �g�U����
+        // 拡散反射
         float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
         
-        // ���ʔ���                                                                                      �� ���̂̋��ʔ��˂̐F�B�����ł͔��ɂ��Ă��� material�Őݒ�ł����肷��Ɨǂ�
+        // 鏡面反射                                                                                      ↓ 物体の鏡面反射の色。ここでは白にしている materialで設定できたりすると良い
         float32_t3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
         
-        // �g�U���� + ���ʔ���
+        // 拡散反射 + 鏡面反射
         output.color.rgb = diffuse + specular;
         //output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.intensity * cos;
-        // �A���t�@�͍��܂Œʂ�
+        // アルファは今まで通り
         output.color.a = gMaterial.color.a * textureColor.a;
         //output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
-    } else { // Lighting���Ȃ��ꍇ�B�O��܂łƓ����v�Z
+    }
+    else
+    { // Lightingしない場合。前回までと同じ計算
         output.color = gMaterial.color * textureColor;
     }
-    output.color = gMaterial.color * textureColor;
-    if (output.color.a == 0.0)
-    {
-        discard;
-    }
-    return output;
-    //output.color = gMaterial.color;
-    //float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
-    //float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
-    //output.color = gMaterial.color * textureColor;
     return output;
 }
