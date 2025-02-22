bba13496b4f0905cfd98aabf9c13d71fd0a9e12f
Atake <146906117+Atake2525@users.noreply.github.com>
2025-02-22 23:32:20 +0900

    ビルド出力先を変更

--- Logger.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-// ログ出力
-namespace Logger {
-void Log(const std::string& message);
-
-void Log(const std::wstring& message);
-}; // namespace Logger
---
+++ project/Engine/Math/Transform.h
@@ -0,0 +1,37 @@
+#include "Vector3.h"
+#include "Vector4.h"
+
+#pragma once
+struct Transform {
+	Vector3 scale;
+	Vector3 rotate;
+	Vector3 translate;
+};
+
+struct DirectionalLight {
+	Vector4 color;     //!< ライトの色
+	Vector3 direction; //!< ライトの向き
+	float intensity;   //!< 輝度
+};
+
+struct PointLight {
+	Vector4 color;    //!< ライトの色
+	Vector3 position; //!< ライトの位置
+	float intensity;  //!< 輝度
+	float radius;     //!< ライトの届く最大距離
+	float dacay;      //!< 減衰率
+};
+
+struct SpotLight {
+	Vector4 color;         //!< ライトの色
+	Vector3 position;      //!< ライトの位置
+	float intensity;       //!< 輝度
+	Vector3 direction;     //!< ライトの向き
+	float distance;        //!< ライトの届く最大距離
+	float dacay;           //!< 減衰率
+	float cosAngle;        //!< スポットライトの余弦
+	float cosFalloffStart; // falloffが開始される角度
+	float padding[2];
+};
+
+//class Transform {};
